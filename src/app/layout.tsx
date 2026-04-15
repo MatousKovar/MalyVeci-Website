93f7f82c93f3be0357bee4d07f3f74eaffa7662c
Matous Kovar <matouskovar1@gmail.com>
2026-04-15 12:16:02 +0200

    imported calendar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "../../components/Navbar";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "MALÝ VĚCI – kapela ze Strakonic a Horažďovic | Oficiální stránky",
@@ -46,7 +50,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <head></head>
       <body>{children}</body>
     </html>
